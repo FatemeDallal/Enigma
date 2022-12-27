bc585a10a3daed94188f228b59d41d4aab2adb59
FatemeDallal <108277235+FatemeDallal@users.noreply.github.com>
2022-12-27 17:08:49 +0330

    method batched for file

---
+++ main.py
@@ -0,0 +1,12 @@
+from itertools import islice
+
+
+def batched(iterable, n):
+    if n < 1:
+        raise ValueError('n must be at least one')
+    it = iter(iterable)
+    while batch := tuple(islice(it, n)):
+        yield batch
+
+
+# .........................................................................................
