edd67f7e919637954dfb215fe892de1a7c6fda74
FatemeDallal <108277235+FatemeDallal@users.noreply.github.com>
2022-12-27 17:11:03 +0330

    search and get enigma with date in file ( work with file )

--- main.py
+++ main.py
@@ -18,3 +18,25 @@ class Enigma:
         self.rotor1 = rotor1
         self.rotor2 = rotor2
         self.rotor3 = rotor3
+
+
+def search_date(date):
+    check = False
+    with open("EnigmaFile.txt") as f:
+        for line in batched(f, 5):
+            if line[0] == "Date: " + date + "\n":
+                plug = line[1][12:-2:].split(", ")
+                plug_board = {}
+                for i in range(len(plug)):
+                    plug_board[plug[i][0]] = plug[i][1]
+                    plug_board[plug[i][1]] = plug[i][0]
+                rotor1 = line[2][9:-2:]
+                rotor2 = line[3][9:-2:]
+                rotor3 = line[4][9:-2:]
+                enigma = Enigma(date, plug_board, rotor1, rotor2, rotor3)
+                check = True
+                break
+    if check:
+        return enigma
+    else:
+        return "not exist"
